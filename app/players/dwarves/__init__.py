73fb048a1f82e8ece931ffab46fe5dd018faee66
matzxcss <matheuslacerda01@proton.me>
2026-01-23 18:38:04 -0300

    fix init

--- app/players/dwarves/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-
-__all__ = ["Dwarf", "DwarfWarrior", "DwarfBlacksmith"]
